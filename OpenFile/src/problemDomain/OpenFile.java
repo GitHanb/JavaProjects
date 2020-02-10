04ee481fbe61834cf3b90a63b2d408ebc0c08581
Henry Han <31779862+GitHanb@users.noreply.github.com>
2020-02-09 21:27:30 -0800

    Uploaded OpenFile project
    Exception handling
    text file stored in the root folder

---
+++ OpenFile/src/problemDomain/OpenFile.java
@@ -0,0 +1,35 @@
+package problemDomain;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
+public class OpenFile
+{
+
+	public static void main(String[] args)
+	{
+		File file;
+		Scanner inputFile;
+		String fileName;
+		
+		fileName = JOptionPane.showInputDialog("Enter the name of a file:");
+		
+		try
+		{
+			file = new File(fileName);
+			inputFile = new Scanner(file);
+			JOptionPane.showMessageDialog(null, "The file was found.");
+		} catch (FileNotFoundException e)
+		{
+			JOptionPane.showMessageDialog(null, "File not found.");
+		}
+		
+		JOptionPane.showMessageDialog(null, "Done");
+		System.exit(0);
+		
+	}
+
+}
